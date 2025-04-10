a4a34b5e57745647856a6cd3f73c173ba1ff774e
Rodrigo Peralta <rodrigoperalta.dev@gmail.com>
2025-04-10 08:40:07 -0300

    chore: setup project

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import enConverter from './locales/en/en'
+import esConverter from './locales/es/es'
+import { Locale, type LocaleConverter } from './types'
+
+const converters: Record<Locale, LocaleConverter> = {
+	[Locale.ENGLISH]: enConverter,
+	[Locale.SPANISH]: esConverter,
+}
+
+export function toWords(num: number, locale: Locale): string {
+	return converters[locale].convert(num)
+}
